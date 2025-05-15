4e3945b6cf06b34a899ba364f510b97173e0b8f7
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-15 15:07:44 +0900

    commit: 日記の一覧、作成、詳細を実装

--- mylog/urls.py
+++ mylog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'mylog'
 urlpatterns = [
     path('',views.LogListView.as_view(), name='log_list'), #ホーム（日記一覧）
-    path('log/add/', views.LogCreateView.as_view(), name='log_create'), #日記作成
-    path('log/<int:pk>/', views.LogDetailView.as_view(), name='log_detail'), # 日記詳細
+    path('create/', views.LogCreateView.as_view(), name='log_create'), #日記作成
+    path('<int:pk>/', views.LogDetailView.as_view(), name='log_detail'), # 日記詳細
     
 ]
