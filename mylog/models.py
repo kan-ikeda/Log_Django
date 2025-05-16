628a326a0219bc2d48523df64dfd0b3db77b5624
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-16 13:39:41 +0900

    作成者をuseridにした

--- mylog/models.py
+++ mylog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,7 +9,7 @@ class Log(models.Model):
     title = models.CharField('タイトル', max_length=100)
     image = models.ImageField('画像', upload_to='logs/', blank=True, null=True)
     date = models.DateField('日付', auto_now_add=True)
-    # author = models.ForeignKey()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     content = models.TextField('内容', blank=True, null=True)
 
     def __str__(self):
