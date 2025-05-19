c82b2229591961983f2003225a95aaffac9b08e8
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-19 10:53:28 +0900

    タグ機能の実装

--- mylog/models.py
+++ mylog/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+#タグモデル
+class Tag(models.Model):
+    name = models.CharField('タグ', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 #日記モデル
 class Log(models.Model):
     title = models.CharField('タイトル', max_length=100)
@@ -11,6 +19,7 @@ class Log(models.Model):
     date = models.DateField('日付', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
     content = models.TextField('内容', blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name='タグ')
 
     def __str__(self):
         return self.title
