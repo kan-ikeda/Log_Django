10c32ee9e0d27d0061eecd34a7d4cf21025df757
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-20 10:33:23 +0900

    ログイン画面のcss実装

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,30 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, required=True, help_text='必須。有効なメールアドレスを入力してください。')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email')
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'birth_date', 'avatar')
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'ユーザー名',
+            'class' : 'form_input'
+        })
+        self.fields['password'].widget.attrs.update({
+            'placeholder': 'パスワード',
+            'class' : 'form_input'
+        })
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,11 +4,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import CustomLoginView
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='mylog:log_list'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
