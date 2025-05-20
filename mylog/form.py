3e1a598c14752cfa2efa51c27386e9806eeea737
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-20 15:32:44 +0900

    投稿画面の画像なしの場合のプレビュー表示

--- mylog/form.py
+++ mylog/form.py
@@ -6,3 +6,28 @@ class LogForm(forms.ModelForm):
     class Meta:
         model = Log
         fields = ('title', 'content', 'image', 'tags')
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-title',
+                'placeholder': 'タイトル',
+                'maxlength': 100,
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-image',
+            }),
+            'date': forms.DateInput(attrs={
+                'class': 'form-date',
+            }),
+            'author': forms.TextInput(attrs={
+                'class': 'form-author',
+                'readonly': 'readonly',
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-content',
+                'rows': 10,
+                'placeholder': 'キャプションを追加',
+            }),
+            'tags': forms.SelectMultiple(attrs={
+                'class': 'form-tags',
+            }),
+        }
