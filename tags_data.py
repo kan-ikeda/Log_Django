10e5bc3ee5791131985c84025bbb0e968385971e
kan-ikeda <github@kan-ikeda@unext-hd.jp>
2025-05-22 10:25:52 +0900

    タグの修正

--- tags_data.py
+++ tags_data.py
@@ -3,7 +3,7 @@ from mylog.models import Tag
 
 
 # タグの作成
-tags = ['日常','旅行','景色','グルメ','仕事','癒し','趣味','健康','趣味']
+tags = ['日常','旅行','景色','グルメ','仕事','癒し','趣味','健康',]
 
 for tag_name in tags:
     Tag.objects.create(name=tag_name)
